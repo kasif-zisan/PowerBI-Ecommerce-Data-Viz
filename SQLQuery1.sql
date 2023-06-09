e59cf2e86481e6f07629b4adc35e33a458aaee57
Kasif Hasnaen Zisan <75033362+kasif-zisan@users.noreply.github.com>
2023-06-09 22:27:13 +0600

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,38 +1,39 @@
-/*
+/* Modifying the database */
 ALTER TABLE dbo.['Ecommerce_Data-1$']
 ADD Month INT
 
 UPDATE dbo.['Ecommerce_Data-1$']
 SET Month = MONTH(Date)
 
-select * from dbo.['Ecommerce_Data-1$']*/
+/* querying the database*/
+select * from dbo.['Ecommerce_Data-1$']
 
 with data as 
 (select Description, Quantity, UnitPrice, CustomerID, Country, Date, Hour, Month from dbo.['Ecommerce_Data-1$'])
 select sum(Quantity*UnitPrice)  from data
 
 
-/*Question 1 - Which country had the most customers?
+/*Question 1 - Which country had the most customers?*/
 select count(CustomerID), Country from data group by Country order by count(CustomerID) desc
-*/
 
-/*Question 2 - Which country generated the most revenue?
+
+/*Question 2 - Which country generated the most revenue?*/
 select sum(UnitPrice*Quantity) as "Total Revenue", Country from data group by Country order by "Total Revenue" desc
-*/
 
-/*Question 3 - Which product sold the most?
+
+/*Question 3 - Which product sold the most?*/
 select Description, sum(Quantity) "Total number sold" from data group by Description order by "Total number sold" desc
-*/
 
-/*Question 4 - Which product generated the most revenue?
+
+/*Question 4 - Which product generated the most revenue?*/
 select description, sum(Quantity*UnitPrice) as "Revenue" from data group by Description order by Revenue desc
-*/
 
-/*Question 5 - Which month had the highest sales?
+
+/*Question 5 - Which month had the highest sales?*/
 select count(Description) "Number of Products Sold", Month from data group by Month order by "Number of Products Sold" desc
-*/
 
-/* Question 6 - Which products were sold most in a certain month?
+
+/* Question 6 - Which products were sold most in a certain month?*/
 WITH CTE AS (
     SELECT Description, SUM(Quantity) AS TotalSold, Month	
     FROM dbo.['Ecommerce_Data-1$']
@@ -45,7 +46,7 @@ FROM (
     FROM CTE
 ) AS T
 WHERE rn = 1
-ORDER BY Month ASC*/
+ORDER BY Month ASC
 
 
 
